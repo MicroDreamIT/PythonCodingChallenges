b3b96f453ca7a9438fbac8fcb79e8c3410e60dde
sharif <shahanur700@gmail.com>
2024-07-31 14:55:10 +0800

    time converter

---
+++ unique_array_converter.py
@@ -0,0 +1,13 @@
+array = [1, 2, a, b, c, a, d, e, f]
+new_array = []
+
+def is_unique(array):
+    pass
+
+
+def unique_array(array):
+    pass
+
+
+if is_unique(new_array):
+    print("Array is unique", new_array)
